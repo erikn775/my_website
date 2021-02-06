0329db0c02f92f3c1e50e0f31f80b6275c3958d1
erikn775 <erikn775@gmail.com>
2021-02-05 21:01:55 -0700

    added linkto repo of webpage

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const xButton = document.getElementById("close")
 const about = document.getElementById("about")
 const gitButton = document.getElementsByClassName("fa fa-github")[0]
 const linkedIn = document.getElementsByClassName("fa fa-linkedin")[0]
+const repoButton = document.getElementById("here_link")
 
+href="https://github.com/erikn775/my_website"
 function div_show() {
     document.getElementById('popupContact').style.display = "block";
 }
@@ -31,6 +33,10 @@ function linkedInRedirect() {
     location.replace("https://www.linkedin.com/in/erik-nielsen-18bb43205")
 }
 
+function gitRepoRedirect() {
+    location.replace("https://github.com/erikn775/my_website")
+}
+
 conButton.addEventListener('click', function() {
     div_show();
     moreOpacity();
@@ -41,15 +47,14 @@ xButton.addEventListener('click', function() {
     lessOpacity();
 })
 
-about.addEventListener('click', function() {
-    let scrollingElement = (document.scrollingElement || document.body);
-    scrollingElement.scrollTop = scrollingElement.scrollHeight;
-})
-
 gitButton.addEventListener('click', function() {
     gitRedirect();
 })
 
 linkedIn.addEventListener('click', function() {
     linkedInRedirect();
+})
+
+repoButton.addEventListener('click', function() {
+    gitRepoRedirect();
 })
