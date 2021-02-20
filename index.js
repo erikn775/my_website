c633950aab01bf0a56816be201369c8a8c4027c2
erikn775 <erikn775@gmail.com>
2021-02-20 14:44:02 -0700

    added links for projects section

--- index.js
+++ index.js
@@ -63,3 +63,51 @@ previous.addEventListener('click', function() {
         projectDisplay();
     }
 })
+
+const linkedin = document.querySelector("#linked")
+    .addEventListener('click', () => location.href = "https://www.linkedin.com/in/erik-nielsen-18bb43205");
+
+const git = document.querySelector("#git")
+    .addEventListener('click', () => location.href = "https://github.com/erikn775?tab=projects")
+
+const projectTitle1 = document.querySelector("#project-1")
+    .addEventListener('click', function(){
+        if (window.confirm('Redirecting to Github')){
+            location.href = "https://github.com/erikn775/rails_project"
+        }
+});
+
+const projectTitle2 = document.querySelector("#project-2")
+    .addEventListener('click', function(){
+        if (window.confirm('Redirecting to Matching Game')){
+            location.href = "https://erikn775.github.io/matching_game_frontend/"
+        }
+});
+
+const projectTitle3 = document.querySelector("#project-3")
+    .addEventListener('click', function(){
+        if (window.confirm('Redirecting to Github')){
+            location.href = "https://github.com/erikn775"
+        }
+});
+
+const projectTitle4 = document.querySelector("#project-4")
+    .addEventListener('click', function(){
+        if (window.confirm('Redirecting to Github')){
+            location.href = "https://github.com/erikn775"
+        }
+});
+
+const projectTitle5 = document.querySelector("#project-5")
+    .addEventListener('click', function(){
+        if (window.confirm('Redirecting to Github')){
+            location.href = "https://github.com/erikn775"
+        }
+});
+
+const projectTitle6 = document.querySelector("#project-6")
+    .addEventListener('click', function(){
+        if (window.confirm('Redirecting to Github')){
+            location.href = "https://github.com/erikn775"
+        }
+});
